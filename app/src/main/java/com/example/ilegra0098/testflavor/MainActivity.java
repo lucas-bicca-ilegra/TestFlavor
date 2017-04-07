ac2aef1b0e3a51372570dde244ebcc864ffb6077
Lucas Bicca <lucaslbs15@gmail.com>
2017-04-06 22:12:11 -0300

    Testing flavors to set a layout for each flavor

--- app/src/main/java/com/example/ilegra0098/testflavor/MainActivity.java
+++ app/src/main/java/com/example/ilegra0098/testflavor/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.ilegra0098.testflavor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        mTextView = (TextView)findViewById(R.id.activity_main_text);
+        mTextView.setText(getString(R.string.description_concept) + " " + BuildConfig.FLAVOR);
     }
 }
